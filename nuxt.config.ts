2906c651bd43e45cbf6addb1e4a64da6ee42a2e3
Anthony Ilinykh <anthonyilinykh@gmail.com>
2022-05-29 19:05:22 +0300

    Add basic webpage metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,23 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  app: {
+    head: {
+      title: `Велодень ${new Date().getFullYear()}`,
+      htmlAttrs: {
+        lang: 'ru',
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'description',
+          content: `Велодень в Орле ${new Date().getFullYear()}`,
+        },
+        { name: 'format-detection', content: 'telephone=no' },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
 })
