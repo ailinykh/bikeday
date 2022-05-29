a596753e19fa6e362b5535283ac5e8707530ad7c
Anthony Ilinykh <anthonyilinykh@gmail.com>
2022-05-29 19:05:57 +0300

    Add ComingSoon component

---
+++ components/ComingSoon.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="soon"></div>
+</template>
+
+<style>
+body {
+  background-color: #579547;
+  background-image: url('/static/bikes-on-green.png');
+  height: 100%;
+}
+
+.soon {
+  height: 310px;
+  width: 100%;
+  background: url('/static/coming-soon.png') center no-repeat;
+  position: absolute;
+  top: 50%;
+  margin-top: -155px;
+}
+</style>
