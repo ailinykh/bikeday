200eb863baf3ac40a7043840346880c3b0dd36a3
Anthony Ilinykh <anthonyilinykh@gmail.com>
2022-05-31 17:26:38 +0300

    Move coming soon to scoped css style

--- components/ComingSoon.vue
+++ components/ComingSoon.vue
@@ -1,14 +1,16 @@
+<script setup>
+useMeta({
+  bodyAttrs: {
+    class: 'body-green',
+  },
+})
+</script>
+
 <template>
   <div class="soon"></div>
 </template>
 
-<style>
-body {
-  background-color: #579547;
-  background-image: url('/static/bikes-on-green.png');
-  height: 100%;
-}
-
+<style scoped>
 .soon {
   height: 310px;
   width: 100%;
