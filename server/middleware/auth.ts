7452d2be9ea0b35cad1a63d67b67b3cd0afe4050
Anthony Ilinykh <anthonyilinykh@gmail.com>
2022-06-05 22:23:33 +0300

    log all the request headers

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,4 +1,5 @@
 export default defineEventHandler(async (event) => {
+  console.log(event.req.headers)
   const { session } = useCookies(event)
   if (!event.req.url.startsWith('/__')) {
     console.log('✅ server auth middleware', event.req.url, 'session:', session)
