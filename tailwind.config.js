40e0a37576c92657b6642e1a6a8312fb5d48a337
Anthony Ilinykh <anthonyilinykh@gmail.com>
2023-08-31 23:57:10 +0400

    Add `flowbite` lib for `tailwindcss` components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./node_modules/flowbite/**/*.{js,ts}"],
   theme: {
     extend: {},
   },
   plugins: [
     require("@tailwindcss/forms"),
     require("@tailwindcss/typography"),
+    require("flowbite/plugin"),
   ],
 };
